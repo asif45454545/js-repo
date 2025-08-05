a42bba3ea309719c09dcf81523a3dee341feed20
asif45454545 <asifakhter7869@gmail.com>
2025-08-05 23:57:32 +0500

    var

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ alert("wellcome to JavaScript");
 
 window.alert("Hi User");
 
-var a = "Ali";
-alert(a);
-console.log(a);
+// var a = "Ali";
+// alert(a);
+// console.log(a);
+
+var a = 10;
+var b = 20;
+console.log(a+b);
