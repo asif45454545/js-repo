7bfbc405ef5112f137c0ccb0ffa04da0395b394c
asif45454545 <asifakhter7869@gmail.com>
2025-08-06 00:04:28 +0500

    addition

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 alert("wellcome to JavaScript");
-
+//   alert box 
 window.alert("Hi User");
 
+//variable use to var 
 // var a = "Ali";
 // alert(a);
 // console.log(a);
 
+//variable use to var addition
+// var a = 10;
+// var b = 20;
+// console.log(a+b);
+
+//variable use to var addition and subtraction
 var a = 10;
 var b = 20;
-console.log(a+b);
+var c = 5;
+console.log((a+b)-c);
